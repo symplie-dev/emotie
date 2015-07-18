a8c7562b2eb5572f78afc2e181ab5f04788799c2
bcdxn <bcdixon@outlook.com>
2015-07-18 17:00:31 -0400

    Remove test data

--- src/app.js
+++ src/app.js
@@ -4,13 +4,6 @@ var React                = require('react'),
     Toast                = require('./components/toast'),
     SettingsModal        = require('./components/modal/settings'),
     EmoticonDetailsModal = require('./components/modal/emoticon-details');
-    
-var testList = [{ name: 'Le Face Face', text: '( ͡° ͜ʖ ͡°)', tags: 'le face face, lenny face, happy, smile' }
-               ,{ name: 'Oh Well', text: '¯\\_(ツ)_/¯', tags: 'oh well, whaatever' }
-               ,{ name: 'Sniper', text: '▄︻̷̿┻̿═━一', tags: 'gun, sniper' }
-               ,{ name: 'Group Lenny', text: '( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)', tags: 'group, lenny face' }
-               ,{ name: 'Glasses', text: '(▀̿Ĺ̯▀̿ ̿)', tags: 'glasses' }
-               ,{ name: 'Over Here', text: '༼ つ ◕_◕ ༽つ', tags: 'over here' }];
 
 React.render(<Header />, document.getElementById('headerContainer'));
 React.render(<Paginater />, document.getElementById('paginaterContainer'));
